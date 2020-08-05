68d023c041d3e125c8954e8bd4e082d0d44cde12
jaco9419 <jaco.9419@gmail.com>
2020-08-05 11:36:24 -0300

    Back to it

--- server/app.js
+++ server/app.js
@@ -1,33 +1,33 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const dotenv = require("dotenv");
+const cors = require('cors');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const dbService = require("./dbService");
+const dbService = require('./dbService');
 
 app.use(cors()); //This will send data to our back-end
 app.use(express.json()); //to send the response in json format
 app.use(express.urlencoded({ extended: false })); //The value will be string or array
 
 //create
-app.post("/insert", (request, response, next) => {
-  console.log(request.body);
+app.post('/insert', (request, response) => {
+    console.log(request.body);
 });
 
 //read
-app.get("/getAll", (request, response, next) => {
-  const db = dbService.getDbServiceInstance();
+app.get('/getAll', (request, response) => {
+    const db = dbService.getDbServiceInstance();
 
-  const result = db.getAllData();
+    const result = db.getAllData();
 
-  result
-    .then((data) => response.json({ data: data }))
-    .catch((err) => console.log(err));
+    result
+        .then((data) => response.json({ data: data }))
+        .catch((err) => console.log(err));
 });
 //update
 
 //delete
 
-app.listen(process.env.PORT, () => console.log("App is running"));
+app.listen(process.env.PORT, () => console.log('App is running'));
